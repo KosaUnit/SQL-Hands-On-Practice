adbac4a89a8307d54ea8867fb337b89acf768e33
KosaKing <Filip.Kosowski@interia.pl>
2024-09-24 23:49:19 +0200

    Add Customer Who Visited but Did Not Make Any Transactions problem solution

--- LeetCode-Problems/2. Basic Joins/customer_who_visited.sql
+++ LeetCode-Problems/2. Basic Joins/customer_who_visited.sql
@@ -1,9 +1,11 @@
--- Problem: Article Views I
--- URL: https://leetcode.com/problems/article-views-i/description/?envType=study-plan-v2&envId=top-sql-50
+-- Problem: Customer Who Visited but Did Not Make Any Transactions
+-- URL: https://leetcode.com/problems/customer-who-visited-but-did-not-make-any-transactions/description/?envType=study-plan-v2&envId=top-sql-50
 -- Difficulty: Easy
 -- Solution:
 
-SELECT DISTINCT V.author_id AS id
-FROM Views V 
-WHERE V.author_id = V.viewer_id
-ORDER BY V.author_id ASC;
+SELECT V.customer_id, COUNT(*) as count_no_trans
+FROM Visits V
+LEFT JOIN Transactions T
+ON V.visit_id = T.visit_id
+WHERE transaction_id IS NULL
+GROUP BY customer_id;
