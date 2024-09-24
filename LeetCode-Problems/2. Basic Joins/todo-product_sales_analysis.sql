1426f9e6e9953ec4177147be9118e74629bd5920
KosaKing <Filip.Kosowski@interia.pl>
2024-09-24 23:52:23 +0200

    Add Product Sales Analysis I problem solution

--- LeetCode-Problems/2. Basic Joins/todo-product_sales_analysis.sql
+++ LeetCode-Problems/2. Basic Joins/todo-product_sales_analysis.sql
@@ -1,9 +1,9 @@
--- Problem: Article Views I
--- URL: https://leetcode.com/problems/article-views-i/description/?envType=study-plan-v2&envId=top-sql-50
+-- Problem: Product Sales Analysis I
+-- URL: https://leetcode.com/problems/product-sales-analysis-i/?envType=study-plan-v2&envId=top-sql-50
 -- Difficulty: Easy
 -- Solution:
 
-SELECT DISTINCT V.author_id AS id
-FROM Views V 
-WHERE V.author_id = V.viewer_id
-ORDER BY V.author_id ASC;
+SELECT P.product_name, S.year, S.price
+FROM Sales S
+LEFT JOIN Product P
+ON P.product_id = S.product_id;
